21e58a79014843350d862e943f5008e7b22050dc
Avin Lad <avinlad@gmail.com>
2020-08-01 19:33:14 -0400

    Added safety algorithm function (not completed)

--- A4-draft.c
+++ A4-draft.c
@@ -8,29 +8,46 @@
 #include <semaphore.h>
 #include <fcntl.h> 
 
+void* customerThread(void* thread); //function for the thread/customer 
+int readFile(char *fileName, int maximum[5][4]);
+int safetyAlgorithm(int nCustomers);
 
-void* customer(void* thread); //function for the thread/customer 
+int P = 5; //processes
+int R = 4  //resources
+int threadCount;
+int safeSequence[R]; 
+int available[R]; //Currently available resources.
+int maximum[P][R];
+int allocation[P][R];
+int need[P][R];
 
 
 int main(int argc, char *argv[]) {
     
-   int available[argc - 1]; //Currently available resources.
-   for (int i=0; i < argc-1; i++){
-       available[i] = atoi(argv[i+1]);
-   }
-   int maximum[5][4];
-   int allocation[5][4];
-   int need[5][4];
-   int threadCount = readFile("sample4_in.txt", &maximum);
-   printf("%d\n", threadCount);
+	if (argc != 5){
+		printf("Please use 4 arguments only.");
+	}
 
-   //Command selection.
-   /*
-   char command[10];
-   while (strcmp(command,"Q") != 0 ){
-       printf("Enter Command (Q to quit): ");
-       scanf("%s",command);
-   } */
+	for (int i=0; i < argc-1; i++){
+		available[i] = atoi(argv[i+1]);
+	}
+
+	threadCount = readFile("sample4_in.txt", maximum);
+	printf("Thread Count %d\n", threadCount);
+
+	for (int j = 0; j < P; j++){
+		for (int k = 0; k < R; k++){
+			allocation[j][k] = 0;
+		}
+	}
+
+	//Command selection.
+	/*
+	char command[10];
+	while (strcmp(command,"Q") != 0 ){
+		printf("Enter Command (Q to quit): ");
+		scanf("%s",command);
+	} */
 }
 
 //function to read file and populate maximum array.
@@ -88,11 +105,58 @@ int readFile(char *fileName, int maximum[5][4]){
 		while(token!=NULL)
 		{
 			maximum[k][j] = atoi(token);
+			allocation[k][j] = atoi(token);
+			need[k][j] = atoi(token);
 			j++;
 			token = strtok(NULL,",");
 		}
 	}
 	return threadCount;
 }
+
+int safetyAlgorithm(int nCustomers){
+	int finish[5] = {1, 1, 1, 1, 1};
+	int safe = 0;
+	int total_customers = nCustomers;
+
+	while (total_customers > 0) {
+        int safe = -1;
+		int execute = 0;
+        for (i = 0; i < P; i++) {
+            if (finish[i]) {
+                execute = 1;
+                for (j = 0; j < R; j++) {
+                    if (maximum[i][j] - allocation[i][j] > available[j]) {
+                        execute = 0;
+                        break;
+                    }
+                }
+ 
+                if (execute == 1) {
+                    printf("\nProcess[%d] is executing.\n", i + 1);
+                    finsih[i] = 0;
+					safeSequence[P - total_customers] = i;
+                    total_customers--;
+                    safe = 0;
+                    for (j = 0; j < R; j++)
+                        available[j] += curr[i][j];
+                    break;
+                }
+            }
+        }
+
+		if (safe == 0){
+			printf("The process is in safe state.\n");
+		}
+		else if (safe == -1){
+			printf("The process is unsafe.\n");
+		}
+
+
+	return safe;
+}
+
+
+
 
 
