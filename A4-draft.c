be5e4947ca5212ba0284135742b36e7414808124
Avin Lad <avinlad@gmail.com>
2020-08-01 22:00:38 -0400

    added command entering

--- A4-draft.c
+++ A4-draft.c
@@ -6,57 +6,99 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <semaphore.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 
-void* customerThread(void* thread); //function for the thread/customer 
+void *customerThread(void *thread); //function for the thread/customer
 int readFile(char *fileName, int maximum[5][4]);
-bool bankersAlgorithm(int nCustomers);
+int bankersAlgorithm(int nCustomers);
 void requestResources(int customer, int r0, int r1, int r2, int r3);
 void releaseResources(int customer, int r0, int r1, int r2, int r3);
 
-int P = 5; //processes
-int R = 4  //resources
+#define P 5 //processes
+#define R 4 //resources
 int threadCount;
-int safeSequence[R]; 
+int safeSequence[R];
 int available[R]; //Currently available resources.
 int maximum[P][R];
 int allocation[P][R];
 int need[P][R];
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv[]) {
-    
-	if (argc != 5){
+	if (argc != 5)
+	{
 		printf("Please use 4 arguments only.");
 	}
 
-	for (int i=0; i < argc-1; i++){
-		available[i] = atoi(argv[i+1]);
+	for (int i = 0; i < argc - 1; i++)
+	{
+		available[i] = atoi(argv[i + 1]);
 	}
 
 	threadCount = readFile("sample4_in.txt", maximum);
-	printf("Thread Count %d\n", threadCount);
+	printf("Number of Customers: %d\n", threadCount);
 
-	for (int j = 0; j < P; j++){
-		for (int k = 0; k < R; k++){
-			allocation[j][k] = 0; // right now nothing is allocated
+	for (int j = 0; j < P; j++)
+	{
+		for (int k = 0; k < R; k++)
+		{
+			allocation[j][k] = 0;		// right now nothing is allocated
 			need[j][k] = maximum[j][k]; // the need will start with max
 		}
 	}
 
-	//Command selection.
-	/*
-	char command[10];
-	while (strcmp(command,"Q") != 0 ){
+	printf("Currently Available Resources: ");
+	for (int i = 0; i < R; i++){
+		printf("%d ", available[i]);
+	}
+	printf("\n");
+
+	//Command selection
+	int customer; 
+	int r0, r1, r2, r3;
+
+	char command[100];
+	while (strcmp(command, "Q") != 0)
+	{
 		printf("Enter Command (Q to quit): ");
-		scanf("%s",command);
-	} */
+		fgets(command, 100, stdin);
+
+		char *p = strtok(command, " ");
+		int i = 0;
+		while (p){
+			if (i == 0){ customer = atoi(p); }
+			else if (i == 1){ r0 = atoi(p); }
+			else if (i == 2){ r1 = atoi(p); }
+			else if (i == 3){ r2 = atoi(p); }
+			else if (i == 4){ r3 = atoi(p); }
+			i++;
+			p = strtok(NULL, " ");
+		}
+
+		if (strstr(command, "RQ")){
+			printf("Processing Resouce Request...\n");
+			requestResources(customer, r0, r1, r2, r3);
+			// printf("User Entered: %d %d %d %d %d\n", customer, r0, r1, r2, r3);
+		}
+		else if (strstr(command, "RL")){
+			printf("rl %d %d %d %d %d\n", customer, r0, r1, r2, r3);
+		}
+		else if (strstr(command, "RUN")){
+			printf("run %d %d %d %d %d\n", customer, r0, r1, r2, r3);
+		}
+		else if (strstr(command, "*")){
+			printf("* %d %d %d %d %d\n", customer, r0, r1, r2, r3);
+		}
+		else if (strstr(command, "Q")){ break; }
+	}
 }
 
 //function to read file and populate maximum array.
-int readFile(char *fileName, int maximum[5][4]){
-    FILE *in = fopen(fileName, "r");
-	if(!in)
+int readFile(char *fileName, int maximum[5][4])
+{
+	FILE *in = fopen(fileName, "r");
+	if (!in)
 	{
 		printf("Child A: Error in opening input file...exiting with error code -1\n");
 		return -1;
@@ -64,139 +106,166 @@ int readFile(char *fileName, int maximum[5][4]){
 
 	struct stat st;
 	fstat(fileno(in), &st);
-	char* fileContent = (char*)malloc(((int)st.st_size+1)* sizeof(char));
-	fileContent[0]='\0';	
-	while(!feof(in))
+	char *fileContent = (char *)malloc(((int)st.st_size + 1) * sizeof(char));
+	fileContent[0] = '\0';
+	while (!feof(in))
 	{
 		char line[100];
-		if(fgets(line,100,in)!=NULL)
+		if (fgets(line, 100, in) != NULL)
 		{
-			strncat(fileContent,line,strlen(line));
+			strncat(fileContent, line, strlen(line));
 		}
 	}
 	fclose(in);
 
-	char* command = NULL;
+	char *command = NULL;
 	int threadCount = 0;
-	char* fileCopy = (char*)malloc((strlen(fileContent)+1)*sizeof(char));
-	strcpy(fileCopy,fileContent);
-	command = strtok(fileCopy,"\r\n");
-	while(command!=NULL)
+	char *fileCopy = (char *)malloc((strlen(fileContent) + 1) * sizeof(char));
+	strcpy(fileCopy, fileContent);
+	command = strtok(fileCopy, "\r\n");
+	while (command != NULL)
 	{
 		threadCount++;
-		command = strtok(NULL,"\r\n");
+		command = strtok(NULL, "\r\n");
 	}
 
-	char* lines[threadCount];
+	char *lines[threadCount];
 	command = NULL;
-	int i=0;
-	command = strtok(fileContent,"\r\n");
-	while(command!=NULL)
+	int i = 0;
+	command = strtok(fileContent, "\r\n");
+	while (command != NULL)
 	{
-		lines[i] = malloc(sizeof(command)*sizeof(char));
-		strcpy(lines[i],command);
+		lines[i] = malloc(sizeof(command) * sizeof(char));
+		strcpy(lines[i], command);
 		i++;
-		command = strtok(NULL,"\r\n");
+		command = strtok(NULL, "\r\n");
 	}
 
-	for(int k=0; k<threadCount; k++)
+	for (int k = 0; k < threadCount; k++)
 	{
-		char* token = NULL;
+		char *token = NULL;
 		int j = 0;
-		token =  strtok(lines[k],",");
-		while(token!=NULL)
+		token = strtok(lines[k], ",");
+		while (token != NULL)
 		{
 			maximum[k][j] = atoi(token);
 			allocation[k][j] = atoi(token);
 			need[k][j] = atoi(token);
 			j++;
-			token = strtok(NULL,",");
+			token = strtok(NULL, ",");
 		}
 	}
 	return threadCount;
 }
 
-int bankersAlgorithm(int nCustomers){
+int bankersAlgorithm(int nCustomers)
+{
 	int finish[5] = {0, 0, 0, 0, 0};
-	bool safe = true;
+	int safe = 0;
 	int total_customers = nCustomers;
 
-	while (total_customers > 0) {
-        int safe = false;
+	while (total_customers > 0)
+	{
+		safe = -1;
 		int execute = 0;
-        for (i = 0; i < P; i++) {
-            if (finish[i] == 0) {
-                execute = 1;
-                for (j = 0; j < R; j++) {
-                    if (need[i][j] > available[j]) {
-                        execute = 0;
-                        break;
-                    }
-                }
+		for (int i = 0; i < P; i++)
+		{
+			if (finish[i] == 0)
+			{
+				execute = 1;
+				for (int j = 0; j < R; j++)
+				{
+					if (need[i][j] > available[j])
+					{
+						execute = 0;
+						break;
+					}
+				}
 
-                if (execute == 1) {
-                    printf("\nProcess[%d] is executing.\n", i + 1);
-                    finish[i] = 1;
+				if (execute == 1)
+				{
+					printf("\nProcess[%d] is executing.\n", i + 1);
+					finish[i] = 1;
 					safeSequence[P - total_customers] = i;
-                    total_customers--;
-                    safe = true;
-                    for (j = 0; j < R; j++)
-                        available[j] += curr[i][j];
-                    break;
-                }
-            }
-        }
-
-		if (safe){
-			printf("\nThe process is in safe state.\n");
+					total_customers--;
+					safe = 0;
+					for (int j = 0; j < R; j++)
+					{
+						available[j] += allocation[i][j];
+					}
+					break;
+				}
+			}
 		}
-		else{
+
+		if (safe == -1)
+		{
 			printf("\nThe process is unsafe.\n");
+			break;
 		}
-
-
+	}
 	return safe;
 }
 
-void requestResources(int customer, int r0, int r1, int r2, int r3){
-	bool canRequest = true;
+void requestResources(int customer, int r0, int r1, int r2, int r3)
+{
+	int canRequest = 1;
 	int i, j;
-	int requestedResources[R]; 
-	
-	requestResources[0] = r0;
-	requestResources[1] = r1;
-	requestResources[2] = r2;
-	requestResources[3] = r3;
+	int requestedResources[R];
 
-	for (i = 0; (i < R) && canRequest; i++){
-		if (requestResources[i] > need[customer][i]){ canRequest = false; }
-		if (requestResources[i] > available[i]) { canRequest = false; }
+	requestedResources[0] = r0;
+	requestedResources[1] = r1;
+	requestedResources[2] = r2;
+	requestedResources[3] = r3;
+
+	for (i = 0; (i < R) && (canRequest == 0); i++)
+	{
+		if (requestedResources[i] > need[customer][i])
+		{
+			canRequest = 0;
+		}
+		if (requestedResources[i] > available[i])
+		{
+			canRequest = 0;
+		}
 	}
 
-	if (!canRequest){
+	if (!canRequest)
+	{
 		printf("Cannot fufill reuqested resourcess.\n");
 	}
-	else{
-		for (i = 0; i < R; i++){ available[i] -= requestResources[i]; }
-
-		for (j = 0; j < R; j++){ 
-			allocation[customer][j] += requestResources[j]; 
-			need[customer][j] -= requestResources[j];
+	else
+	{
+		for (i = 0; i < R; i++)
+		{
+			available[i] -= requestedResources[i];
 		}
 
-		bool isSafe = bankersAlgorithm(threadCount);
+		for (j = 0; j < R; j++)
+		{
+			allocation[customer][j] += requestedResources[j];
+			need[customer][j] -= requestedResources[j];
+		}
 
-		if (!isSafe){
-			for (i = 0; i < R; i++){ available[i] += requestResources[i]; }
+		int isSafe = bankersAlgorithm(threadCount);
 
-			for (j = 0; j < R; j++){ 
-				allocation[customer][j] -= requestResources[j]; 
-				need[customer][j] += requestResources[j];
+		if (isSafe == 0)
+		{
+			for (i = 0; i < R; i++)
+			{
+				available[i] += requestedResources[i];
+			}
+
+			for (j = 0; j < R; j++)
+			{
+				allocation[customer][j] -= requestedResources[j];
+				need[customer][j] += requestedResources[j];
 			}
 
 			printf("Cannot fufill reuqested resourcess. Please wait patiently.\n");
 		}
-		else{
+		else
+		{
 			printf("Reuqested Resources Successful.\n");
 		}
 	}
@@ -204,28 +273,36 @@ void requestResources(int customer, int r0, int r1, int r2, int r3){
 	return;
 }
 
-void releaseResources(int customer, int r0, int r1, int r2, int r3){
-	bool canRelease = true;
+void releaseResources(int customer, int r0, int r1, int r2, int r3)
+{
+	int canRelease = 1;
 	int i, j;
-	int releasedResources[R]; 
-	
+	int releasedResources[R];
+
 	releasedResources[0] = r0;
 	releasedResources[1] = r1;
 	releasedResources[2] = r2;
 	releasedResources[3] = r3;
 
-	for (i = 0; i < R; i++){
-		if (releaseResources[i] > allocation[customer][R]){ canRelease = false; }
+	for (i = 0; (i < R) && (canRelease == 1); i++)
+	{
+		if (releasedResources[i] > allocation[customer][i])
+		{
+			canRelease = 0;
+		}
 	}
 
-	if (!canRelease){
+	if (!canRelease)
+	{
 		printf("Cannot complete release of resources.\n");
 	}
-	else{
-		for (j = 0; j < R; j++){
-			available[j] += releaseResources[j];
-			need[customer][j] += releaseResources[j];
-			allocation[customer][j] -= releaseResources[j];
+	else
+	{
+		for (j = 0; j < R; j++)
+		{
+			available[j] += releasedResources[j];
+			need[customer][j] += releasedResources[j];
+			allocation[customer][j] -= releasedResources[j];
 		}
 
 		printf("Resources Released Successfully.\n");
@@ -233,8 +310,3 @@ void releaseResources(int customer, int r0, int r1, int r2, int r3){
 
 	return;
 }
-
-
-
-
-
